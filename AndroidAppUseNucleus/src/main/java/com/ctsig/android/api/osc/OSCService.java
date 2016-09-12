1b21113aff9d730d0b788279e9d523cb7990db5d
shenzhaoxiang <shenzhaoxiang@gmail.com>
2016-09-12 17:55:50 +0800

    重构retrofit。

--- AndroidAppUseNucleus/src/main/java/com/ctsig/android/api/osc/OSCService.java
+++ AndroidAppUseNucleus/src/main/java/com/ctsig/android/api/osc/OSCService.java
@@ -1,15 +1,5 @@
 package com.ctsig.android.api.osc;
 
-import com.ctsig.android.data.model.entity.User;
-import com.ctsig.android.data.net.request.CreateAuthorization;
-import com.ctsig.android.data.net.response.AuthorizationResp;
-
-import retrofit2.http.Body;
-import retrofit2.http.GET;
-import retrofit2.http.POST;
-import retrofit2.http.Query;
-import rx.Observable;
-
 /**
  * @version V1.0
  * @des
@@ -18,10 +8,10 @@ import rx.Observable;
  * @date: 2016-08-24 17:29
  */
 public interface OSCService {
-    @POST("/action/api/login_validate")
-    Observable<RespUser> login(
-            @Query("username") String username,
-            @Query("pwd") String password,
-            @Query("keep_login") int keepLogin
-    );
+//    @POST("/action/api/login_validate")
+//    Observable<RespUser> login(
+//            @Query("username") String username,
+//            @Query("pwd") String password,
+//            @Query("keep_login") int keepLogin
+//    );
 }
